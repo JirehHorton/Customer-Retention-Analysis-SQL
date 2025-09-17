c86139d7885d3cc9ed30c4e23ee3b3b31fe6a4d0
Jireh Horton <jirehhorton25@gmail.com>
2025-09-17 14:36:32 -0700

    Delete sql directory

--- sql/create_tables.sql
+++
@@ -1,8 +0,0 @@
-Create table customers(
-		    customer_id VARCHAR(60),
-        customer_unique_id VARCHAR(60),
-        customer_name VARCHAR(50),
-        customer_zip_code_prefix INT,
-        customer_city VARCHAR (35),
-		    customer_state VARCHAR (3)
-  );
