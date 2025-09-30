62bf31fccbb2a74a9c3d4bdbefca153189bbe514
Jireh Horton <jirehhorton25@gmail.com>
2025-09-30 00:53:56 -0700

    Update queries.sql

--- 📂 sql/queries.sql
+++ 📂 sql/queries.sql
@@ -1,1 +1,17 @@
+      -- QUERY #1 New Customers Per Month --
 
+SELECT DATE_FORMAT(first_order, "%Y-%c") AS first_month,
+COUNT(DISTINCT customer_id) AS new_customers
+FROM ( 
+	SELECT c.customer_id,
+		   MIN(order_delivered_customer_date) AS first_order
+	FROM customers c
+	JOIN orders o 
+		ON c.customer_id = o.customer_id
+	WHERE order_status = 'delivered'
+	GROUP BY customer_id
+) AS sub
+GROUP BY first_month
+ORDER BY MIN(first_order) DESC;
+
+       -- QUERY #2 --
