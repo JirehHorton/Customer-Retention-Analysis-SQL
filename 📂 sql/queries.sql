b188f7501f0bacc8d916d3b927a7b8d2132f5c13
Jireh Horton <jirehhorton25@gmail.com>
2025-10-04 02:30:24 -0700

    Update queries.sql

--- 📂 sql/queries.sql
+++ 📂 sql/queries.sql
@@ -23,3 +23,27 @@ FROM (
 	GROUP BY customer_unique_id) AS customer_orders
 WHERE count_oi >= 2
 ;
+
+		-- QUERY #3 Cohort Analysis  --
+SELECT cohort_month, COUNT(DISTINCT customer_unique_id) AS returning_customers
+FROM (
+    SELECT 
+        c.customer_unique_id,
+        DATE_FORMAT(f.cohort_date, '%Y-%m') AS cohort_month,
+        DATE_FORMAT(o.order_delivered_customer_date, '%Y-%m') AS order_month
+    FROM customers c
+    JOIN orders o 
+      ON c.customer_unique_id = o.customer_unique_id
+    JOIN (
+        SELECT customer_unique_id, MIN(order_delivered_customer_date) AS cohort_date
+        FROM orders
+        WHERE order_status = 'delivered'
+        GROUP BY customer_unique_id
+    ) f
+      ON c.customer_unique_id = f.customer_unique_id
+    WHERE o.order_status = 'delivered'
+) AS sub
+WHERE order_month > cohort_month
+GROUP BY cohort_month
+ORDER BY cohort_month DESC;
+
